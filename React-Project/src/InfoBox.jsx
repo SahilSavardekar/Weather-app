905c49e0ac4ca708226a4f45d247edea17181a37
Sahil Savardekar <sahilsavardekar2003@gmail.com>
2024-03-23 21:19:51 +0530

    created a weather app

---
+++ React-Project/src/InfoBox.jsx
@@ -0,0 +1,44 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+
+export default function InfoBox( {info}){
+    const img_url = "https://images.unsplash.com/photo-1584267385494-9fdd9a71ad75?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    const HOT_URL="https://images.unsplash.com/photo-1431882697221-ccdceda3a7d7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL="https://images.unsplash.com/photo-1457269449834-928af64c684d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const RAIN_URL="https://plus.unsplash.com/premium_photo-1661715304145-e686bb947289?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    return(
+        <div className="InfoBox">
+            <div className='CardContainer'>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
+                    title="green iguana"
+                />
+                
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {info.city} {info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <WbSunnyIcon/> : <AcUnitIcon/>}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" component={"span"}>
+                        <p>Temperature = {info.temp}&deg;C</p>
+                        <p>Humidity = {info.humidity}</p>
+                        <p>Min temp = {info.tempMin}&deg;C</p>
+                        <p>Max temp = {info.tempMax}&deg;C</p>
+                        <p>The weather can be described as <i>{info.weather}</i> and feels like {info.feelsLike}&deg;C</p>
+                    </Typography>
+                </CardContent>
+            </Card>
+            </div>
+
+        </div>
+    )
+}
